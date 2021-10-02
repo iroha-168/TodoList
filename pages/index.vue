7843a5d0e4c69af1c69cf109f9f8e5dbf87abde2
mi_iroha <chimosy21@gmail.com>
2021-10-02 23:17:41 +0900

    feat:編集機能を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="input_todo">
-      <v-form>
+      <v-form onsubmit="return false;">
         <v-container>
           <v-row>
             <v-col md="7">
@@ -10,7 +10,10 @@
                 v-model="content"
                 label="Todo"
                 type=text
-                required>
+                required
+                @keydown.enter.exact.prevent
+                @keyup.enter.exact="editTodo"
+              >
               </v-text-field>
               <v-row justify="end">
                 <v-btn color="primary" @click="addTodo">追加</v-btn>
@@ -23,9 +26,19 @@
 
     <div class="todo_list">
       <ul type="cirsle">
-        <li v-for="todo in todos" :key="todo.id">{{ "content: " + todo.content + " ,id: " + todo.id }} 
-          <span @click="deleteTodo(todo.id)">✖️</span>
-        </li>
+        <v-card 
+          v-for="todo in todos" :key="todo.id"
+          max-width="400">
+          {{ todo.content }} 
+
+          <v-btn icon color="blue" @click='deleteTodo(todo.id)'>
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+
+          <v-btn icon color="blue" @click='returnToForm(todo.id)'>
+            <v-icon>mdi-pencil-outline</v-icon>
+          </v-btn>
+        </v-card>
       </ul>
     </div>
 
@@ -41,8 +54,9 @@ export default {
   setup() {
     // TextFieldに入力された文字列を取得する
     const id = ref(0)
-    const content = ref('')
+    const content = ref('') // 入力フォームに入力された文字が入る
     const todos = ref([])
+    const targetEditId = ref(null)
 
     const addTodo = () => {
       todos.value.push({id: id.value, content: content.value})
@@ -54,12 +68,35 @@ export default {
       todos.value = todos.value.filter(todo => todo.id !== targetId)
     }
 
+    // 選択されたtodoを入力フォームに表示させる
+    const returnToForm = (targetId) => {
+      targetEditId.value = targetId
+
+      todos.value.forEach((todo) => {
+        if(todo.id === targetId) {
+          content.value = todo.content
+        }
+      })
+    }
+
+    // エンターキー押下でtodosを編集する
+    const editTodo = () => {
+      todos.value.map((todo) => {
+        if(todo.id === targetEditId.value) {
+          todo.content = content.value
+          content.value = ""
+        }
+        return todos
+      })
+    }
+
     return {
       addTodo,
       todos,
       content,
-
-      deleteTodo
+      deleteTodo,
+      returnToForm,
+      editTodo
     }
   }
 }
